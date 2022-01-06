1c294b529d5acea86bcca5946d807a65c3862c06
Shelly Lachish <shellstar@gmail.com>
2022-01-06 17:06:46 +0000

    updates prep of valid projects file

--- R/load_prep_data.R
+++ R/load_prep_data.R
@@ -17,17 +17,27 @@ library(recommenderlab)
 user_counts <- fread(here('input_data', '2021_user_classification_counts.csv'))
 user_firstlast <- fread(here('input_data', '2021_user_first_last_classification.csv'))
 valid_projects <- fread(here('input_data', 'launch_approved_projects_20-Dec-2021.csv'), header = F,
-                        col.names = c("project_id", "slug")) %>%
-                  mutate(exists = TRUE)
+                        col.names = c("project_id", "slug"))
+
+#Clean up the valid projects dataset
+#resp matches the word at the start of the string, an optional space, then the rest of the string.
+#The parenthesis are subexpressions accessed as backreferences \\1 and \\2.
+#rexp <- "^(.*?)\\s(.*)$"
+
+valid_projects_details <- valid_projects %>%
+  separate(slug, into = c("slug_end", "name"), sep = "\\s", extra = "merge") %>%
+  mutate(url = paste0("zooniverse.org/projects/", slug_end))
+valid_projects_details
+
+#Save the project list
+saveRDS(valid_projects_details, file = here("input_data", "project_details.rds"))
 
 #***************************************
 #Merge into one data set
 zoo_data_full <- user_counts %>%
   left_join(user_firstlast, by = c("user_id", "project_id")) %>%
-  left_join(valid_projects, by = "project_id") %>%
   #Keep only valid projects (removes beta testing projects)
-  filter(exists == T) %>%
-  select(-exists, -slug) %>%
+  filter(project_id %in% valid_projects$project_id) %>%
   arrange(user_id, project_id)
 
 #zoo_data_full
@@ -42,6 +52,5 @@ zoo_data <- zoo_data_full %>%
   ungroup()
 #zoo_data
 
-
-rm(user_counts, user_firstlast, zoo_data_full)
+rm(user_counts, user_firstlast, zoo_data_full, valid_projects)
 
