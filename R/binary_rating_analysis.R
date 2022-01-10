68da79d88b7cc1d557347bf18631b042161e79b2
Shelly Lachish <shellstar@gmail.com>
2022-01-10 13:59:24 +0000

    applies styler styling

--- R/binary_rating_analysis.R
+++ R/binary_rating_analysis.R
@@ -1,38 +1,36 @@
-#Load libraries and data
+# Load libraries and data
 library(here)
 
 source(here("R", "load_prep_data.R"))
-#loads objects: "valid_projects_details" "zoo_data"
+# loads objects: "valid_projects_details" "zoo_data"
 
 #********************************************
-#Create the BINARY ratings matrix
-#user_ids_vec <- zoo_data %>% pull(user_id)
+# Create the BINARY ratings matrix
+# user_ids_vec <- zoo_data %>% pull(user_id)
 
 zoo_wide <- zoo_data %>%
-  pivot_wider(id_cols = user_id,
-              names_from = project_id,
-              values_from = num_classifications,
-              names_glue = "P.{project_id}",
-              values_fill = NA) %>%
-  #we make the user_id col into rownames
+  pivot_wider(
+    id_cols = user_id,
+    names_from = project_id,
+    values_from = num_classifications,
+    names_glue = "P.{project_id}",
+    values_fill = NA
+  ) %>%
+  # we make the user_id col into rownames
   column_to_rownames("user_id")
 
-# Need to save the wide version for use in web-app as
-# = only point where user ids and prefs exist together
-saveRDS(zoo_wide, here("input_data", "user_project_matrix.rds"))
-
-#Convert to recommenderlab binary matrix
+# Convert to Recommenderlab binary matrix
 zoo_ratings <- as.matrix(zoo_wide)
 real_ratings <- as(zoo_ratings, "realRatingMatrix")
 binary_ratings <- binarize(real_ratings, minRating = 1)
 binary_ratings
 
-#image(binary_ratings[1:50, 1:50])
+# image(binary_ratings[1:50, 1:50])
 
 #********************************************
-#Look for and remove Outliers
+# Look for and remove Outliers
 
-#distributions of the number of users per project:
+# distributions of the number of users per project:
 n_users <- colCounts(binary_ratings)
 qplot(n_users) +
   stat_bin(binwidth = 100) +
@@ -40,48 +38,53 @@ qplot(n_users) +
 qplot(n_users[n_users < 8000 & n_users > 5]) + stat_bin(binwidth = 3)
 
 # Remove projects with < 5 users
-binary_ratings_filtered <- binary_ratings[, colCounts(binary_ratings) >= 5 ]
+binary_ratings_filtered <- binary_ratings[, colCounts(binary_ratings) >= 5]
 binary_ratings_filtered
 
-#Now we have some users with no projects
-#sum(rowCounts(binary_ratings_filtered) == 0)
-#So only keep users that have classified on at least 5 projects
+# Now we have some users with no projects
+# sum(rowCounts(binary_ratings_filtered) == 0)
+
+# So only keep users that have classified on at least 5 projects
 binary_ratings_filtered <- binary_ratings_filtered[rowCounts(binary_ratings_filtered) >= 5, ]
 binary_ratings_filtered
 
-#projects_per_users <- rowCounts(binary_ratings_filtered)
-#range(projects_per_users)
-#qplot(projects_per_users) +  stat_bin(binwidth = 1)
-#qplot(projects_per_users[projects_per_users < 20 ]) + stat_bin(binwidth = 1)
+# projects_per_users <- rowCounts(binary_ratings_filtered)
+# range(projects_per_users)
+# qplot(projects_per_users) +  stat_bin(binwidth = 1)
+# qplot(projects_per_users[projects_per_users < 20 ]) + stat_bin(binwidth = 1)
 
-#Save the filtered preference matrix to use in the web app
-saveRDS(binary_ratings_filtered, file = here("input_data", "binary_preference_matrix.rds"))
-
+# Save the filtered preference matrix to use in the web app
+saveRDS(binary_ratings_filtered,
+        file = here("input_data", "binary_preference_matrix.rds"))
 
 #********************************************
 # Run an assortment of algorithms to see which works best
 
 scheme <- binary_ratings_filtered %>%
-  evaluationScheme(method = "cross",
-                   k      = 4,
-                   train  = 0.8,
-                   given  = -1)
-
-algorithms <- list(
-  #"association rules" = list(name = "AR", param = list(supp = 0.1, conf = 0.01)),
-  "random"  = list(name = "RANDOM", param = NULL),
-  "popular" = list(name  = "POPULAR", param = NULL),
-  "IBCF_10" = list(name  = "IBCF", param = list(k = 5)), # k=10 or 30 did worse then k = 5
-  "UBCF_nn500" = list(name  = "UBCF", param = list(method = "Cosine", nn = 50)) # nn = 100 and 500 did worse
+  evaluationScheme(
+    method = "cross",
+    k = 4,
+    train = 0.8,
+    given = -1
   )
 
-results <- recommenderlab::evaluate(scheme,
-                                    algorithms,
-                                    type  = "topNList",
-                                    n     = c(1, 3, 5, 10, 15, 20)
+algorithms <- list(
+  "association rules" = list(name = "AR", param = list(supp = 0.1, conf = 0.01)),
+  "random"  = list(name = "RANDOM", param = NULL),
+  "popular" = list(name = "POPULAR", param = NULL),
+  # For IBCF: k = 10 or 30 did worse then k = 5
+  "IBCF_10" = list(name = "IBCF", param = list(k = 5)),
+  # For UBCF: nn = 100 and 500 did worse than nn = 50
+  "UBCF_nn500" = list(name  = "UBCF", param = list(method = "Cosine", nn = 50))
 )
 
-#View results from each of the algorithms in turn
+results <- recommenderlab::evaluate(scheme,
+  algorithms,
+  type  = "topNList",
+  n     = c(1, 3, 5, 10, 15, 20)
+)
+
+# View results from each of the algorithms in turn
 results$random %>% getConfusionMatrix()
 results$IBCF %>% getConfusionMatrix()
 
@@ -90,13 +93,13 @@ results$IBCF %>% getConfusionMatrix()
 
 # see here for details: https://diegousai.io/2019/03/market-basket-analysis-part-2-of-3/
 
-#Put the previous steps into a function (to use with map() below)
+# Put the previous steps into a function (to use with map() below)
 avg_conf_matr <- function(results) {
   tmp <- results %>%
-    getConfusionMatrix()  %>%
+    getConfusionMatrix() %>%
     as.list()
-  as.data.frame( Reduce("+",tmp) / length(tmp)) %>%
-    select('n', 'precision', 'recall', 'TPR', 'FPR')
+  as.data.frame(Reduce("+", tmp) / length(tmp)) %>%
+    select("n", "precision", "recall", "TPR", "FPR")
 }
 
 # Using map() to iterate function across all models
@@ -107,33 +110,39 @@ results_tbl <- results %>%
   # Unnesting to have all variables on same level
   unnest(cols = c(value))
 
-#Classification models performance can be compared using the ROC curve,
+# Classification models performance can be compared using the ROC curve,
 # which plots the true positive rate (TPR) against the false positive rate (FPR).
 
 results_tbl %>%
   ggplot(aes(FPR, TPR, colour = fct_reorder2(as.factor(name), FPR, TPR))) +
   geom_line() +
-  geom_label(aes(label = n))  +
-  labs(title = "ROC curves",
-       colour = "Model") +
+  geom_label(aes(label = n)) +
+  labs(
+    title = "ROC curves",
+    colour = "Model"
+  ) +
   theme_grey(base_size = 14)
 
 
 results_tbl %>%
   ggplot(aes(recall, precision,
-             colour = fct_reorder2(as.factor(name),  precision, recall))) +
+    colour = fct_reorder2(as.factor(name), precision, recall)
+  )) +
   geom_line() +
-  geom_label(aes(label = n))  +
-  labs(title = "Precision-Recall curves",
-       colour = "Model") +
+  geom_label(aes(label = n)) +
+  labs(
+    title = "Precision-Recall curves",
+    colour = "Model"
+  ) +
   theme_grey(base_size = 14)
 
 
 #***********************************************
 # Build the IBCF model
-recomm <- Recommender(getData(scheme, 'train'),
-                      method = "IBCF",
-                      param = list(k = 5))
+recomm <- Recommender(getData(scheme, "train"),
+  method = "IBCF",
+  param = list(k = 5)
+)
 
 recomm
 
@@ -144,13 +153,16 @@ recomm
 valid_projects <- valid_projects %>%
   mutate(in_model = ifelse(project_id %in% str_remove(model_project_ids, "P."), 1, 0))
 
-#These are random animal projects:
+# These are random animal projects:
 (random <- c(11, 14, 75, 4179))
-#These are random astrophysics projects:
+
+# These are random astrophysics projects:
 (random <- c(39, 73, 1338))
-#These are random arts/not science projects:
+
+# These are random arts/not science projects:
 (random <- c(4405, 5042, 5352, 6406))
-#These are Cam's ones:
+
+# These are a specific users ones:
 (random <- zoo_data %>%
   filter(user_id == 6) %>%
   pull(project_id) %>%
@@ -167,7 +179,9 @@ newdat <- data.frame(project_id = model_project_ids, value = 0) %>%
   # Convert to recommenderlab class 'binaryRatingsMatrix'
   as("binaryRatingMatrix")
 
-pred_projids <- as(predict(recomm, newdata = newdat, n = 5), 'list')$`1`
+pred_projids <- as(predict(recomm, newdata = newdat, n = 5), "list")$`1`
+
+
 (pred_projects <- valid_projects %>%
   mutate(new_project_id = paste0("P.", project_id)) %>%
   filter(new_project_id %in% pred_projids) %>%
@@ -175,8 +189,8 @@ pred_projids <- as(predict(recomm, newdata = newdat, n = 5), 'list')$`1`
 
 
 #*****************************************************
-#Function to test random predictions
-test_predictions <- function(valid_projects, model_project_ids){
+# Function to test random predictions
+test_predictions <- function(valid_projects, model_project_ids) {
   random_proj_ids <- sample(model_project_ids, 5)
 
   user_details <- valid_projects %>%
@@ -186,7 +200,7 @@ test_predictions <- function(valid_projects, model_project_ids){
   message("Chosen user has classified on these projects:")
   print(user_details)
 
-  #create the new data binary rating matrix
+  # create the new data binary rating matrix
   newdat <- data.frame(project_id = model_project_ids, value = 0) %>%
     mutate(value = ifelse(project_id %in% random_proj_ids, 1, 0)) %>%
     # Spread into sparse matrix format
@@ -204,7 +218,6 @@ test_predictions <- function(valid_projects, model_project_ids){
 
   message("\n", "Projects predicted for user:")
   print(pred_projects)
-
 }
 
 test_predictions(valid_projects, model_project_ids)
--- R/load_prep_data.R
+++ R/load_prep_data.R
@@ -1,56 +1,58 @@
-#Load and prep data
+# Load and prep data
 
-#load data wrangling packages
+# load data wrangling packages
 library(data.table)
 library(tidyverse)
 library(lubridate)
 library(skimr)
 
-#load path setting package
+# load path setting package
 library(here)
 
-#load Recommenderlab
+# load Recommenderlab
 library(recommenderlab)
 
 #***************************************
-#Load Datasets
-user_counts <- fread(here('input_data', '2021_user_classification_counts.csv'))
-user_firstlast <- fread(here('input_data', '2021_user_first_last_classification.csv'))
-valid_projects <- fread(here('input_data', 'launch_approved_projects_20-Dec-2021.csv'), header = F,
-                        col.names = c("project_id", "slug"))
+# Load Datasets
+user_counts <- fread(here("input_data", "2021_user_classification_counts.csv"))
+user_firstlast <- fread(here("input_data", "2021_user_first_last_classification.csv"))
+valid_projects <- fread(here("input_data", "launch_approved_projects_20-Dec-2021.csv"),
+  header = F,
+  col.names = c("project_id", "slug")
+)
 
-#Clean up the valid projects dataset
-#resp matches the word at the start of the string, an optional space, then the rest of the string.
-#The parenthesis are subexpressions accessed as backreferences \\1 and \\2.
-#rexp <- "^(.*?)\\s(.*)$"
+# Clean up the valid projects dataset
+# rexp matches the word at the start of the string, an optional space, then the rest of the string.
+# the ? after .* makes it a not-greedy expression
+# The parenthesis are subexpressions accessed as backreferences \\1 and \\2.
+# rexp <- "^(.*?)\\s(.*)$"
 
 valid_projects_details <- valid_projects %>%
   separate(slug, into = c("slug_end", "name"), sep = "\\s", extra = "merge") %>%
   mutate(url = paste0("zooniverse.org/projects/", slug_end))
 valid_projects_details
 
-#Save the project list
+# Save the project list
 saveRDS(valid_projects_details, file = here("input_data", "project_details.rds"))
 
 #***************************************
-#Merge into one data set
+# Merge into one data set
 zoo_data_full <- user_counts %>%
   left_join(user_firstlast, by = c("user_id", "project_id")) %>%
-  #Keep only valid projects (removes beta testing projects)
+  # Keep only valid projects (removes beta testing projects)
   filter(project_id %in% valid_projects$project_id) %>%
   arrange(user_id, project_id)
 
-#zoo_data_full
-#skim(zoo_data_full)
+# zoo_data_full
+# skim(zoo_data_full)
 
 #***************************************
-#Update dataset without timestamps and with counts per project rather than per workflow_id
+# Update dataset without timestamps and with counts per project rather than per workflow_id
 zoo_data <- zoo_data_full %>%
   select(-created_at, -updated_at, -workflow_id) %>%
   group_by(user_id, project_id) %>%
   summarise(num_classifications = sum(num_subjects)) %>%
   ungroup()
-#zoo_data
+# zoo_data
 
 rm(user_counts, user_firstlast, zoo_data_full, valid_projects)
-
