044d46df0445c26c7fef8609a367ecc77415a323
Campbell Allen <campbell.allen@gmail.com>
2022-01-13 11:16:57 +0000

    Api integration additions (#2)
    * add CORS headers for open access API
    
    allow cross domain web requests to allow this API to be used in other websites
    
    * add page_size param
    
    allow the calling client to set the num of results they want, default to 5 if missing.

--- R/recommend_projects.R
+++ R/recommend_projects.R
@@ -4,20 +4,29 @@ library(recommenderlab)
 # Source script to load data and recommender model
 source(here("R", "data_model_prep.R"))
 
+#* @filter cors
+cors <- function(res) {
+  # we could lock this down to only the service origins we expect traffic from
+  # e.g www.zooniverse.org vs * (allow all)
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  plumber::forward()
+}
+
 #* Return recommendations for a specified user
 #* @param user_id The user_id for which to predict project recommendations
 #* @get /recommend
-function(user_id) {
+function(user_id, page_size=5) {
 
   # User_id needs to be character type
   user_id <- as.character(user_id)
+  page_size <- as.character(page_size)
 
   # get the user data to predict for
   newdat <- preference_matrix[
     preference_matrix@data@itemsetInfo$itemsetID == user_id,
   ]
 
-  model_predictions <- predict(recomm_model, newdata = newdat, n = 5)
+  model_predictions <- predict(recomm_model, newdata = newdat, n = page_size)
 
   pred_projects <- as.numeric(gsub(
     "P.", "",
